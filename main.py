1d38d573b85f18eb3f36e9fb3d285dec68810f1a
Luewngier <g217881111@gmail.com>
2023-08-30 22:57:00 +0800

    Add backward left and right functions and listeners

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ def forward():
     tim.forward(10)
 
 
+def backward():
+    tim.backward(10)
+
+
+def left():
+    tim.left(5)
+
+
+def right():
+    tim.right(5)
+
+
 screen.listen()
-screen.onkey(key="space", fun=forward)
+screen.onkey(key="w", fun=forward)
+screen.onkey(key="s", fun=backward)
+screen.onkey(key="d", fun=right)
+screen.onkey(key="a", fun=left)
+
 screen.exitonclick()
