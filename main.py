a15a15bad7836bf33af22571c9d93f94ba2dbb6f
Luewngier <g217881111@gmail.com>
2023-08-31 23:37:38 +0800

    Add turtle group to screen

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
+rainbow = ["red", "orange", "yellow", "green", "blue", "purple"]
+turtle_group = []
 
 
-def forward():
-    tim.forward(10)
+def join_turtle(order: int):
+    new_tim = Turtle()
+    new_tim.penup()
+    new_tim.color(rainbow[i])
+    new_tim.goto(x=-230, y=(order * 60 - 150))
+    turtle_group.append(new_tim)
 
 
-def backward():
-    tim.backward(10)
+for i in range(6):
+    join_turtle(i)
 
-
-def left():
-    tim.left(5)
-
-
-def right():
-    tim.right(5)
-
-
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
-
-
-screen.listen()
-screen.onkey(key="w", fun=forward)
-screen.onkey(key="s", fun=backward)
-screen.onkey(key="d", fun=right)
-screen.onkey(key="a", fun=left)
-screen.onkey(key="c", fun=clear)
 
 screen.exitonclick()
