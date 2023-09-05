aa013f6320292534cc8cb3e28890e1ae93fd462c
Luewngier <g217881111@gmail.com>
2023-09-05 23:55:59 +0800

    Create initial sneak

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.colormode(255)
+
+snake = []
+
+for i in range(3):
+    newPoint = Turtle(shape="square")
+    newPoint.color(255, 255, 255)
+    newPoint.setx(i * 20)
+    snake.append(newPoint)
 
 
 screen.exitonclick()
