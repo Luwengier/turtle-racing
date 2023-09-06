494154989231950e5df44dc4c91391aa46d5c21d
Luewngier <g217881111@gmail.com>
2023-09-06 23:29:32 +0800

    Make sneak move forward

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.colormode(255)
+screen.tracer(0)
+
 
 snake = []
 
 for i in range(3):
-    newPoint = Turtle(shape="square")
-    newPoint.color(255, 255, 255)
-    newPoint.setx(i * 20)
-    snake.append(newPoint)
+    new_segment = Turtle(shape="square")
+    new_segment.penup()
+    new_segment.speed(1)
+    new_segment.color("white")
+    new_segment.setx(i * -20)
+    snake.append(new_segment)
+
+game_is_on = True
+
+while game_is_on:
+    for seg in snake:
+        seg.forward(20)
+    screen.update()
+    time.sleep(0.5)
 
 
 screen.exitonclick()
