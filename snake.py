502f86a4fb5c1af3a9b8006f77d309e5d1c286f9
Luewngier <g217881111@gmail.com>
2023-09-12 21:29:50 +0800

    Control direction of snake

--- snake.py
+++ snake.py
@@ -1,24 +1,35 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+
 
 class Snake:
     def __init__(self) -> None:
-        self.snake = []
+        self.segment = []
 
         for i in range(3):
             new_segment = Turtle(shape="square")
             new_segment.penup()
             new_segment.color("white")
-            new_segment.setx(i * -20)
-            self.snake.append(new_segment)
+            new_segment.setx(i * -MOVE_DISTANCE)
+            self.segment.append(new_segment)
 
     def move(self):
-        for seg_num in range(len(self.snake) - 1, 0, -1):
-            new_x = self.snake[seg_num - 1].xcor()
-            new_y = self.snake[seg_num - 1].ycor()
-            self.snake[seg_num].goto(new_x, new_y)
+        for seg_num in range(len(self.segment) - 1, 0, -1):
+            new_x = self.segment[seg_num - 1].xcor()
+            new_y = self.segment[seg_num - 1].ycor()
+            self.segment[seg_num].goto(new_x, new_y)
 
-        self.snake[0].forward(20)
-        # snake[0].left(90)
-        # snake[0].forward(20)
-        # snake[0].right(90)
+        self.segment[0].forward(MOVE_DISTANCE)
+
+    def right(self):
+        self.segment[0].setheading(0)
+
+    def up(self):
+        self.segment[0].setheading(90)
+
+    def left(self):
+        self.segment[0].setheading(180)
+
+    def down(self):
+        self.segment[0].setheading(270)
